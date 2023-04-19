5082eba68406d2064bcc3d7db3d4b77054e3fdfc
Vinayak Shastri <vkshastri6929@gmail.com>
2023-04-19 13:01:40 +0530

    ImageGallery

---
+++ tars/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import H from './components/routing'
+import { BrowserRouter as Router } from 'react-router-dom'
+
+function App() {
+  return (
+    <Router>
+  <H /></Router>
+  );
+}
+
+export default App;
